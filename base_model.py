e4ac09238f32b9974f55fe30242c2a818d1f4fb3
semquant <lihezizou@gmail.com>
2017-08-28 14:18:40 -0400

    constructing net

--- base_model.py
+++ base_model.py
@@ -14,13 +14,13 @@ def variable_summaries(var, name):
     """Attach a lot of summaries to a Tensor."""
     with tf.name_scope('summaries'):
         mean = tf.reduce_mean(var)
-        tf.scalar_summary('mean/' + name, mean)
+        tf.summary.scalar('mean/' + name, mean)
         with tf.name_scope('stddev'):
             stddev = tf.sqrt(tf.reduce_sum(tf.square(var - mean)))
-        tf.scalar_summary('sttdev/' + name, stddev)
-        tf.scalar_summary('max/' + name, tf.reduce_max(var))
-        tf.scalar_summary('min/' + name, tf.reduce_min(var))
-        tf.histogram_summary(name, var)
+        tf.summary.scalar('sttdev/' + name, stddev)
+        tf.summary.scalar('max/' + name, tf.reduce_max(var))
+        tf.summary.scalar('min/' + name, tf.reduce_min(var))
+        tf.summary.histogram(name, var)
 
 
 class conv_net():
@@ -30,7 +30,7 @@ class conv_net():
                         "He": tf.contrib.layers.variance_scaling_initializer(factor=2.0, mode='FAN_IN', uniform=False)}
         self.variables = {}
 
-    def nn_layer(input_dim, output_dim, layer_name, initializer="xavier"):
+    def nn_layer(self, input_dim, output_dim, layer_name, initializer="xavier"):
         initializer = self.izdict[initializer]
         with tf.variable_scope(layer_name):
             with tf.variable_scope("weight"):
@@ -46,47 +46,50 @@ class conv_net():
 
     def __set_variable__(self, initializer, strides=[1,2,1,2]):
 
-        full_size = int(self.input_size/(strides[1]**2)/(strides[3]**2))
-        nn_layer([5,5,1], [32], "conv1", initializer)
-        nn_layer([5,5,32], [64], "conv2", initializer)
-        nn_layer([full_size*64], [1024], "fcon1", initializer)
-        nn_layer([1024], [self.n_output], "output", initializer)
+        full_size = int(self.n_input/(strides[1]**2)/(strides[3]**2))
+        self.nn_layer([5,5,1], [32], "conv1", initializer)
+        self.nn_layer([5,5,32], [64], "conv2", initializer)
+        self.nn_layer([full_size*64], [1024], "fcon1", initializer)
+        self.nn_layer([1024], [self.n_output], "output", initializer)
 
     def network(self, x, n_input, n_output, dropout, strides, initializer="xavier"):
         self.n_input = n_input
         self.n_output = n_output
         self.__set_variable__(initializer=initializer, strides=strides)
 
-        x = tf.reshape(x, shape=[-1, np.sqrt(n_input), np.sqrt(n_input), 1])
+        x = tf.reshape(x, shape=[-1, int(np.sqrt(n_input)), int(np.sqrt(n_input)), 1])
         with tf.variable_scope('conv1'):
             # Convolution Layer
             conv1 = conv2d(x, self.variables["conv1_w"], self.variables["conv1_b"], strides = strides[0])
-            tf.histogram_summary(self.name+"/conv1/conv_output", conv1)
+            tf.summary.histogram(self.name+"/conv1/conv_output", conv1)
             # Max Pooling (down-sampling)
             conv1 = maxpool2d(conv1, k=strides[1])
-            tf.histogram_summary(self.name+"/conv1/maxpool_output", conv1)
+            tf.summary.histogram(self.name+"/conv1/maxpool_output", conv1)
         with tf.variable_scope('conv2'):
             # Convolution Layer
             conv2 = conv2d(conv1, self.variables["conv2_w"], self.variables["conv2_b"], strides = strides[2])
-            tf.histogram_summary(self.name+"/conv2/conv_output", conv2)
+            tf.summary.histogram(self.name+"/conv2/conv_output", conv2)
             # Max Pooling (down-sampling)
             conv2 = maxpool2d(conv2, k=strides[3])
-            tf.histogram_summary(self.name+"/conv2/maxpool_output", conv2)
+            tf.summary.histogram(self.name+"/conv2/maxpool_output", conv2)
         with tf.variable_scope('fcon1'):
             # Fully connected layer
             # Reshape conv2 output to fit fully connected layer input
             fc1 = tf.reshape(conv2, [-1, self.variables["fcon1_w"].get_shape().as_list()[0]])
             fc1 = tf.add(tf.matmul(fc1, self.variables["fcon1_w"]), self.variables["fcon1_b"])
-            tf.histogram_summary(self.name+"/fcon1/fcon1_output", fc1)
+            tf.summary.histogram(self.name+"/fcon1/fcon1_output", fc1)
             fc1 = tf.nn.relu(fc1)
             # Apply Dropout
             fc1 = tf.nn.dropout(fc1, dropout)
-            tf.histogram_summary(self.name+"/fcon1/dropout_output", fc1)
+            tf.summary.histogram(self.name+"/fcon1/dropout_output", fc1)
         with tf.variable_scope('output'):
             # Output
             self.out = tf.add(tf.matmul(fc1, self.variables["output_w"]), self.variables["output_b"])
-            tf.histogram_summary(self.name+"/output/preact_output", self.out)
+            tf.summary.histogram(self.name+"/output/preact_output", self.out)
 
         return self.out
+
+
+
 
 
--- mv_nn.py
+++ mv_nn.py
@@ -5,6 +5,38 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 from base_model import conv_net, variable_summaries
 
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+
+def cost_func(pred_mean, pred_var, y):
+    scaler = tf.constant([-1,1], shape=[2,1], dtype=tf.float32)
+    m_y = tf.matmul(y,scaler)
+
+    dist = tf.contrib.distributions.Normal(loc=pred_mean, scale=pred_var)
+    f = lambda x: 1/(1+tf.exp(-tf.multiply(x,m_y)))
+    out = tf.contrib.bayesflow.monte_carlo.expectation(f=f,p=dist,n=1000)
+
+    out = -tf.reduce_mean(tf.log(out + 1e-8))
+    return out
+
+def gradient_modify(pred_mean, pred_var, y, sample_size):
+    scaler = tf.constant([1,-1], shape=[2,1], dtype=tf.float32)
+    m_y = tf.matmul(y,scaler)
+    dist = tf.contrib.distributions.Normal(loc=pred_mean, scale=pred_var)
+
+    f1 = lambda x: 1/(1+tf.exp(-tf.multiply(x,m_y)))
+    f2 = lambda x: tf.multiply(1/(1+tf.exp(-tf.multiply(x,m_y))),tf.div(x-pred_mean, pred_var**2))
+    f3 = lambda x: tf.multiply(1/(1+tf.exp(-tf.multiply(x,m_y))),tf.div((pred_mean-x)**2, pred_var**3)-1/(pred_var**2))
+
+    denominator = tf.contrib.bayesflow.monte_carlo.expectation(f=f1,p=dist,n=1000)
+    numerator1 = tf.contrib.bayesflow.monte_carlo.expectation(f=f2,p=dist,n=1000)
+    numerator2 = tf.contrib.bayesflow.monte_carlo.expectation(f=f3,p=dist,n=1000)
+    sample_size = tf.cast(sample_size, tf.float32)
+    out1 = tf.div(tf.div(numerator1,denominator), sample_size)
+    out2 = tf.div(tf.div(numerator2,denominator), sample_size)
+
+    return out1, out2
+
 class mean_var_net():
     def __init__(self, data, n_input = 784, n_classes = 2):
         self.data = data
@@ -16,65 +48,102 @@ class mean_var_net():
             self.x = tf.placeholder(tf.float32, shape=[None, self.n_input],name='x')
             self.y = tf.placeholder(tf.float32, shape=[None, self.n_classes],name='y')
 
-        with tf.variables_scope('hyperparameter'):
+        with tf.variable_scope('hyperparameter'):
             self.keep_prob = tf.placeholder(tf.float32, name='dropout')
+            tf.summary.scalar('dropout_keep_probability', self.keep_prob)
             self.learning_rate = tf.placeholder(tf.float32, name='learning_rate')
+            tf.summary.scalar('learning_rate', self.learning_rate)
 
-        with tf.variables_scope('mean_net'):
+        with tf.variable_scope('mean_net'):
             self.mean_net = conv_net('mean_net')
-            self.mean_net.network(x=self.x, n_input=self.n_input, n_output=self.n_classes-1,
-                                  dropout=self.keep_prob, strides=strides[:4],initializer="xavier")
-            with tf.variables_scope('output'):
-                self.mean_out = self.mean_net.out
+            self.mean_net.network(x=self.x, n_input=self.n_input,
+                                  n_output=self.n_classes-1, dropout=self.keep_prob,
+                                  strides=strides[:4],initializer="xavier")
 
-        with tf.variables_scope('var_net'):
+        with tf.variable_scope('var_net'):
             self.var_net = conv_net('var_net')
-            self.var_net.network(x=self.x, n_input=self.n_input, n_output=self.n_classes-1,
-                                  dropout=self.keep_prob, strides=strides[4:],initializer="xavier")
-            with tf.variables_scope('output'):
-                self.var_out = 10*tf.sigmoid(self.mean_net.out)
-                tf.histogram_summary("var_net/output/act_output", self.var_out)
+            self.var_net.network(x=self.x, n_input=self.n_input,
+                                 n_output=self.n_classes-1, dropout=self.keep_prob,
+                                 strides=strides[4:],initializer="xavier")
+            with tf.variable_scope('output'):
+                self.var_net.out = 10*tf.sigmoid(self.mean_net.out)
+                tf.summary.histogram("var_net/output/act_output", self.var_net.out)
 
     def __define_measure__(self):
         self.sample_size = tf.placeholder(tf.int32)
-        with tf.variables_scope('accuracy'):
+        with tf.variable_scope('accuracy'):
+            class_score = tf.concat([tf.zeros([self.sample_size, 1], tf.float32),
+                                     self.mean_net.out],1)
+            with tf.variable_scope('correct_prediction'):
+                correct_pred = tf.equal(tf.argmax(class_score, 1),
+                                        tf.argmax(self.y, 1))
+            with tf.variable_scope('accuracy'):
+                accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+            tf.summary.scalar('accuracy', accuracy)
+
+        with tf.variable_scope('cost'):
+            with tf.variable_scope('virtual_cost'):
+                self.virtual_cost = tf.add(self.mean_net.out,self.var_net.out)
+            with tf.variable_scope('true_cost'):
+                self.cost = cost_func(self.mean_net.out,self.var_net.out,self.y)
 
 
     def __optimization__(self, learning_rate=0.001):
+        with tf.variable_scope('optimization'):
+            optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+            with tf.variable_scope("variables"):
+                self.mean_variable = list(self.mean_net.variables.values())
+                self.var_variable = list(self.var_net.variables.values())
+            with tf.variable_scope("gradient"):
+                self.grad_mod1,self.grad_mod2 = gradient_modify(self.mean_net.out, self.var_net.out,
+                                                                self.y, self.sample_size)
+                variable_summaries(self.grad_mod1, "optimization/gradient/mean_gradient")
+                variable_summaries(self.grad_mod2, "optimization/gradient/var_gradient")
+
+                gradient1 = optimizer.compute_gradients(loss=self.virtual_cost,
+                                                        var_list=self.mean_variable,
+                                                        grad_loss=self.grad_mod1)
+                gradient2 = optimizer.compute_gradients(loss=self.virtual_cost,
+                                                        var_list=self.var_variable,
+                                                        grad_loss=self.grad_mod1)
+                self.apply_gradients = optimizer.apply_gradients(gradient1+gradient2)
 
     def train_net(self, training_iters = 20000, learning_rate = 0.001,
                    batch_size = 100, display_step = 1, dropout = 0.75,
                    strides = [1,1,2,2,1,1,2,2]):
 
         # Initializing the variables
+        self.__construct_net__(strides=strides)
+        self.__define_measure__()
+        self.__optimization__(learning_rate=learning_rate)
+        init = tf.global_variables_initializer()
+        merged = tf.summary.merge_all()
+
         # Launch the graph
         with tf.Session() as sess:
             sess.run(init)
             step = 1
-            batch_x, batch_y = self.data.train.next_batch(batch_size)
+            train_writer = tf.summary.FileWriter('../summary/train',
+                                                  sess.graph)
+
             # Keep training until reach max iterations
             while step * batch_size <= training_iters:
-
+                batch_x, batch_y = self.data.train.next_batch(batch_size)
                 # Run optimization op (backprop)
-                #if step %2 == 1:
-                mean,var = sess.run([self.mean_net,self.var_net], feed_dict={self.x: batch_x,
-                                                self.y: batch_y,
-                                                self.keep_prob: 1.,
-                                                self.sample_size: batch_y.shape[0]})
-
-                sess.run(self.apply_gradients, feed_dict={self.x: batch_x,
-                                                          self.y: batch_y,
-                                                          self.keep_prob: dropout,
-                                                          self.sample_size: batch_y.shape[0]})
-
+                summary, _ = sess.run([merged, self.apply_gradients], feed_dict={self.x: batch_x,
+                                                                                 self.y: batch_y,
+                                                                                 self.keep_prob: dropout,
+                                                                                 self.learning_rate: learning_rate,
+                                                                                 self.sample_size: batch_y.shape[0]})
+                train_writer.add_summary(summary, step)
                 # display training intermediate result
 
                 if step % display_step == 0:
                     # Calculate batch loss and accuracy
                     loss, acc = sess.run([self.cost,self.accuracy], feed_dict={self.x: batch_x,
-                                                                                self.y: batch_y,
-                                                                                self.sample_size: batch_y.shape[0],
-                                                                                self.keep_prob: 1.})
+                                                                               self.y: batch_y,
+                                                                               self.sample_size: batch_y.shape[0],
+                                                                               self.keep_prob: 1.})
                     print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + \
                           "{:.6f}".format(loss) + ", Training Accuracy= " + \
                           "{:.5f}".format(acc))
