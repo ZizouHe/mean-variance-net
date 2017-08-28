0c7036fa11e1718ba136f80324c28aae2bf94a1a
semquant <lihezizou@gmail.com>
2017-08-28 17:47:11 -0400

    Construction finished

--- base_model.py
+++ base_model.py
@@ -1,36 +1,128 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 27 19:06:24 2017
+
+@author: Zizou
+
+Construct basic CNN model
+"""
+
 import tensorflow as tf
 import numpy as np
 
 def conv2d(x, W, b, strides=1):
+    """
+    Convolution operator 2D wrapper, with bias and relu activation.
+    Use the padding = "SAME" will produce output with same size as input.
+
+    Parameters
+    ----------
+    x: tensor, input data/output from last layer
+    W: weight matrix for a small convolution mask
+    b: bias matrix
+    strides: strides, here we set columns stride = row stride
+
+    Returns
+    -------
+    a tensor with conv and relu operator
+    """
+    # Conv2D wrapper, with bias and relu activation
+    # input a*a, output a*a: padding = "SAME"
     x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')
     x = tf.nn.bias_add(x, b)
     return tf.nn.relu(x)
 
 def maxpool2d(x, k=2):
+    """
+    MaxPool operator 2D wrapper.
+    down-sampling procedure: input a*a, output a/k * a/k.
+
+    Parameters
+    ----------
+    x: tensor, input data/output from last layer
+    k: max_pool size and strides size
+
+    Returns
+    -------
+    a down-sample tensor
+    """
     return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],
                           padding='SAME')
 
 def variable_summaries(var, name):
-    """Attach a lot of summaries to a Tensor."""
+    """
+    Attach a lot of summaries to a Tensor.
+    Used in Tensorboard;
+    Generate mean, min and max values.
+
+    Parameters
+    ----------
+    var: tensor to be summarized
+    name: tensor name/summary path
+    """
     with tf.variable_scope('summaries'):
         mean = tf.reduce_mean(var)
         tf.summary.scalar('mean/' + name, mean)
-        with tf.variable_scope('stddev'):
-            stddev = tf.sqrt(tf.reduce_sum(tf.square(var - mean)))
-        tf.summary.scalar('sttdev/' + name, stddev)
+        #with tf.variable_scope('stddev'):
+        #    stddev = tf.sqrt(tf.reduce_sum(tf.square(var - mean)))
+        #tf.summary.scalar('sttdev/' + name, stddev)
         tf.summary.scalar('max/' + name, tf.reduce_max(var))
         tf.summary.scalar('min/' + name, tf.reduce_min(var))
         tf.summary.histogram(name, var)
 
 
 class conv_net():
+    """
+    Convolution neural network for mean/variance estimation.
+
+    Network Structure:
+    1. Input data: 2D images k*k
+    2. Conv1 layer: convolution layer with bias and ReLU op, total d1 mask;
+                   output size: k*K*d1
+    3. Maxpool layer: maxpool with stride size=k1 and ksize=k1;
+                      output size: (k/k1)*(k/k1)*d1
+    4. Conv2 layer: convolution layer with bias and ReLU op, total d2 mask;
+                   output size: (k/k1)*(k/k1)*d2
+    5. Maxpool layer: maxpool with stride size=k2 and ksize=k2;
+                      output size: [k/(k1*k2)]*[k/(k1*k2)]*d2
+    6. fully-connected1 layer: linear and ReLU op, dropout rate = dropout;
+                               output size: 1024*1
+    7. fully-connected output layer: linear op only, dropout rate = 1;
+                                     output size: class_number*1(or class_number-1 * 1)
+    """
     def __init__(self, name):
+        """
+        Initialize network.
+
+        Parameters
+        ----------
+        name: network name
+
+        Attributes
+        ----------
+        name: network name
+        izdict: initializer dict, currently includes Xavier's and He's initialization methods
+        variables: variables dict of whole network
+        """
         self.name = name
         self.izdict = {"xavier": tf.contrib.layers.xavier_initializer(uniform=True),
-                        "He": tf.contrib.layers.variance_scaling_initializer(factor=2.0, mode='FAN_IN', uniform=False)}
+                        "He": tf.contrib.layers.variance_scaling_initializer(factor=2.0, mode='FAN_IN',
+                                                                             uniform=False)}
         self.variables = {}
 
     def nn_layer(self, input_dim, output_dim, layer_name, initializer="xavier"):
+        """
+        Reusable code for making a simple neural net layer.
+        Construct with structure that can be shown in Tensorboard.
+
+        Parameters
+        ----------
+        input_dim: input dimension, list type
+        output_dim: output dimension, list type
+        layer_name: layer name
+        initializer: the choice of initialization method for layer weights and biases
+        """
         initializer = self.izdict[initializer]
         with tf.variable_scope(layer_name):
             with tf.variable_scope("weight"):
@@ -41,10 +133,12 @@ class conv_net():
             with tf.variable_scope("bias"):
                 self.variables[layer_name+'_b'] = tf.get_variable(name= 'bias',shape=output_dim,
                                                                   dtype=tf.float32, initializer=initializer)
+        # Record variable summaries
         variable_summaries(self.variables[layer_name+'_w'], self.name+"/"+layer_name+'/weight')
         variable_summaries(self.variables[layer_name+'_b'], self.name+"/"+layer_name+'/bias')
 
     def __set_variable__(self, initializer, strides=[1,2,1,2]):
+        """Set up network's variables"""
 
         full_size = int(self.n_input/(strides[1]**2)/(strides[3]**2))
         self.nn_layer([5,5,1], [32], "conv1", initializer)
@@ -53,6 +147,32 @@ class conv_net():
         self.nn_layer([1024], [self.n_output], "output", initializer)
 
     def network(self, x, n_input, n_output, dropout, strides, initializer="xavier"):
+        """
+        Construct Convolution network.
+
+        Parameters
+        ----------
+        x: tensors, input data
+        n_input: input size
+        n_output: output size
+        dropout: dropout rate in fully-connected layer
+        strides: numpy array with 4 elements
+            1. conv layer 1 stride size
+            2. max_pool layer 1 ksize and strides size
+            3. conv layer 2 stride size
+            4. max_pool layer 2 ksize and strides size
+        initializer: the choice of initialization method
+
+        Attributes
+        ----------
+        n_input: input size
+        n_output: output size
+        out: output tensor
+
+        Returns
+        -------
+        a output tensor
+        """
         self.n_input = n_input
         self.n_output = n_output
         self.__set_variable__(initializer=initializer, strides=strides)
@@ -88,8 +208,3 @@ class conv_net():
             tf.summary.histogram(self.name+"/output/preact_output", self.out)
 
         return self.out
-
-
-
-
-
--- mv_nn.py
+++ mv_nn.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 27 19:27:03 2017
+
+@author: Zizou
+
+Construct mean-variance net
+"""
+
 from mnist import MNIST
 import numpy as np
 import scipy
@@ -5,45 +15,127 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 from base_model import conv_net, variable_summaries
 
-flags = tf.app.flags
-FLAGS = flags.FLAGS
 
 def cost_func(pred_mean, pred_var, y):
+    """
+    Custom cost function for mean-variance train.
+    Our cost function is:
+    $\sum_{i = 1}^n ln (E(\frac{1}{1 + e^{-yx}} | x \sim N[\mu; \sigma^2]))$
+    Because cost function contains integral terms, we do not compute it directly;
+    Instead, we use Monte-Carlo Simulation;
+    We then use Monte-Carlo result to estimate the expectation = integral term.
+
+    Parameters
+    ----------
+    pred_mean: a sample_size*(n_classes-1) matrix provides prob for all but one classes
+    pred_var: a sample_size*(n_classes-1) matrix provides variance for all but one classes
+    y: a sample_size*n_classes one-hot matrix provides labels
+    note that pred_mean actually provides class score
+
+    Returns
+    -------
+    a tensor
+    """
     scaler = tf.constant([-1,1], shape=[2,1], dtype=tf.float32)
+    # reshape y from n_size*n_class to n_size*(n_class-1) then calculate -y*x
     m_y = tf.matmul(y,scaler)
 
     dist = tf.contrib.distributions.Normal(loc=pred_mean, scale=pred_var)
     f = lambda x: 1/(1+tf.exp(-tf.multiply(x,m_y)))
+    # Monte-Carlo expectation
     out = tf.contrib.bayesflow.monte_carlo.expectation(f=f,p=dist,n=1000)
 
     out = -tf.reduce_mean(tf.log(out + 1e-8))
     return out
 
 def gradient_modify(pred_mean, pred_var, y, sample_size):
+    """
+    Gradient modify function.
+    Calculate the last step gradient: gradient on cost function;
+    Our gradient for mean net output is:
+    $$
+    \frac{1}{K}\sum_{i = 1}^K \frac{E\left\{\frac{1}{1 + \mathrm{e}^{-y_it}}
+    \frac{t - \mu_(x_i)}{\sigma^2(x_i)}|\; t \sim N\left[\mu(x_i), \sigma^2(x_i)\right]\right\}}
+    {E\left\{\frac{1}{1 + \mathrm{e}^{-y_it}} |\; t \sim N\left[\mu(x_i), \sigma^2(x_i)\right]\right\}}
+    \frac{\partial \mu(x_i)}{\partial w_\mu}
+    $$
+
+    Our gradient for var net output is:
+    $$
+    \frac{1}{K}\sum_{i = 1}^K \frac{E\left\{\frac{1}{1 + \mathrm{e}^{-y_it}}
+    \left[\frac{\left(t - \mu_(x_i)\right)^2}{\sigma^3(x_i)} \, - \frac{1}{\sigma(x_i)}\right]
+    |\; t \sim N\left[\mu(x_i), \sigma^2(x_i)\right]\right\}}{E\left\{\frac{1}{1 + \mathrm{e}^{-y_it}}
+    |\; t \sim N\left[\mu(x_i), \sigma^2(x_i)\right]\right\}} \frac{\partial \sigma(x_i)}{\partial w_\sigma}
+    $$
+
+    Parameters
+    ----------
+    pred_mean: a sample_size*(n_classes-1) matrix provides prob for all but one classes
+    pred_var: a sample_size*(n_classes-1) matrix provides variance for all but one classes
+    y: a sample_size*n_classes one-hot matrix provides labels
+    note that pred_mean actually provides class score
+    sample_size: sample size
+
+    Returns
+    -------
+    a tuple of tensors, gradient of mean and variance net
+    Note that each tensor has shape: [sample_size,1]
+    """
     scaler = tf.constant([1,-1], shape=[2,1], dtype=tf.float32)
+    # reshape y from n_size*n_class to n_size*(n_class-1) then calculate -y*x
     m_y = tf.matmul(y,scaler)
     dist = tf.contrib.distributions.Normal(loc=pred_mean, scale=pred_var)
 
     f1 = lambda x: 1/(1+tf.exp(-tf.multiply(x,m_y)))
     f2 = lambda x: tf.multiply(1/(1+tf.exp(-tf.multiply(x,m_y))),tf.div(x-pred_mean, pred_var**2))
-    f3 = lambda x: tf.multiply(1/(1+tf.exp(-tf.multiply(x,m_y))),tf.div((pred_mean-x)**2, pred_var**3)-1/(pred_var**2))
+    f3 = lambda x: tf.multiply(1/(1+tf.exp(-tf.multiply(x,m_y))),
+                               tf.div((pred_mean-x)**2, pred_var**3)-1/(pred_var**2))
 
     denominator = tf.contrib.bayesflow.monte_carlo.expectation(f=f1,p=dist,n=1000)
     numerator1 = tf.contrib.bayesflow.monte_carlo.expectation(f=f2,p=dist,n=1000)
     numerator2 = tf.contrib.bayesflow.monte_carlo.expectation(f=f3,p=dist,n=1000)
+
     sample_size = tf.cast(sample_size, tf.float32)
+    # gradient for mean net
     out1 = tf.div(tf.div(numerator1,denominator), sample_size)
+    # gradient for var net
     out2 = tf.div(tf.div(numerator2,denominator), sample_size)
 
     return out1, out2
 
 class mean_var_net():
+    """Mean and Variance Network for Classification."""
     def __init__(self, data, n_input = 784, n_classes = 2):
+        """
+        Initialize network
+
+        Parameters
+        ----------
+        data: object from a data class,
+              have method mini-batch, attributes train, test, validation
+        n_input: data input size(e.g. img shape: 28*28 = 784)
+        n_classes: total classes number(e.g. 0-9 digits; 0/1 labels)
+        """
         self.data = data
         self.n_input = n_input
         self.n_classes = n_classes
 
     def __construct_net__(self, strides=[1,2,1,2,1,2,1,2]):
+        """
+        Construct the structure of network.
+        Record summaries to be shown in Tensorboard.
+
+        Attributes
+        ----------
+        x: input data
+        y: label data
+        keep_prob: dropout
+        learning_rate: learning rate
+        mean_net: mean network
+        var_net: var network
+        mean: mean network output
+        var: var network output
+        """
         with tf.variable_scope('input'):
             self.x = tf.placeholder(tf.float32, shape=[None, self.n_input],name='x')
             self.y = tf.placeholder(tf.float32, shape=[None, self.n_classes],name='y')
@@ -56,22 +148,43 @@ class mean_var_net():
 
         with tf.variable_scope('mean_net'):
             self.mean_net = conv_net('mean_net')
+            # Use He's initialization
             self.mean_net.network(x=self.x, n_input=self.n_input,
                                   n_output=self.n_classes-1, dropout=self.keep_prob,
-                                  strides=strides[:4],initializer="xavier")
+                                  strides=strides[:4],initializer="He")
+
+            with tf.variable_scope('mean_net'):
+                # Add activation function on output, scale mean output to [-10,10]
+                self.mean = 10*tf.tanh(self.mean_net.out)
+                variable_summaries(self.mean, "values")
 
         with tf.variable_scope('var_net'):
             self.var_net = conv_net('var_net')
+            # Use Xavier's initialization
             self.var_net.network(x=self.x, n_input=self.n_input,
                                  n_output=self.n_classes-1, dropout=self.keep_prob,
-                                 strides=strides[4:],initializer="xavier")
+                                 strides=strides[4:], initializer="xavier")
+
             with tf.variable_scope('output'):
-                self.var_net.out = 10*tf.sigmoid(self.var_net.out)
-                tf.summary.histogram("var_net/output/act_output", self.var_net.out)
+                # Add activation function on output, scale var output to [0.03,10.03]
+                self.var = 10*tf.sigmoid(self.var_net.out)+0.03
+                variable_summaries(self.var, 'values')
 
     def __define_measure__(self):
+        """
+        Define cost and accuracy in mean var net.
+        Record summaries to be shown in Tensorboard.
+
+        Attributes
+        ----------
+        accuracy: classification accuracy
+        virtual_cost: fake cost used to compute gradient in previous steps
+        cost: actual cost, computation needs Monte-Carlo simulation
+        validation_cost: cost function value on validation set
+        """
         self.sample_size = tf.placeholder(tf.int32)
         with tf.variable_scope('accuracy'):
+            # compute class score
             class_score = tf.concat([tf.zeros([self.sample_size, 1], tf.float32),
                                      self.mean_net.out],1)
             with tf.variable_scope('correct_prediction'):
@@ -82,22 +195,34 @@ class mean_var_net():
             tf.summary.scalar('accuracy', self.accuracy)
 
             with tf.variable_scope('virtual_cost'):
-                self.virtual_cost = tf.add(self.mean_net.out,self.var_net.out)
+                self.virtual_cost = tf.add(self.mean,self.var)
             with tf.variable_scope('true_cost'):
-                self.cost = cost_func(self.mean_net.out,self.var_net.out,self.y)
-                self.validation_cost = cost_func(self.mean_net.out,self.var_net.out,self.y)
+                self.cost = cost_func(self.mean,self.var,self.y)
+                self.validation_cost = cost_func(self.mean,self.var,self.y)
             tf.summary.scalar('train_cost', self.cost)
             tf.summary.scalar('validation_cost', self.validation_cost)
 
 
     def __optimization__(self, learning_rate=0.001):
+        """
+        Define optimization methods for networks.
+        Use fake cost to calculate gradients on previous steps;
+        Manually calculate the gradients on last step(actual cost);
+        Combine the gradients and apply on Adam optimizer.
+
+        Attributes
+        ----------
+        grad_mod1: mean net gradient on acutual cost function
+        grad_mod2: var net gradient on acutual cost function
+        apply_gradients: apply real gradients on network weights and biases
+        """
         with tf.variable_scope('optimization'):
             optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
             with tf.variable_scope("variables"):
                 self.mean_variable = list(self.mean_net.variables.values())
                 self.var_variable = list(self.var_net.variables.values())
             with tf.variable_scope("gradient"):
-                self.grad_mod1,self.grad_mod2 = gradient_modify(self.mean_net.out, self.var_net.out,
+                self.grad_mod1,self.grad_mod2 = gradient_modify(self.mean, self.var,
                                                                 self.y, self.sample_size)
 
                 gradient1 = optimizer.compute_gradients(loss=self.virtual_cost,
@@ -111,11 +236,32 @@ class mean_var_net():
             variable_summaries(self.grad_mod1, "gradient/mean_gradient")
             variable_summaries(self.grad_mod2, "gradient/var_gradient")
 
-    def train_net(self, training_iters = 20000, learning_rate = 0.001,
-                   batch_size = 100, display_step = 1, dropout = 0.75,
-                   strides = [1,2,1,2,1,2,1,2]):
+    def train_net(self, training_iters = 20000, learning_rate = 0.001, batch_size = 100,
+                  display_step = 1, dropout = 0.75, strides = [1,2,1,2,1,2,1,2]):
+        """
+        Train conv nets: mean and variance CNN.
+        Currently, the two network does not share parameters.
+        During each optimization process,
+        we update parameters in two nets simultaneously.
 
-        # Initializing the variables
+        Parameters
+        ----------
+        training_iters: number of iterations during training
+        learning_rate: learning rate
+        batch_size: data size of mini-batch
+        display_step: how many steps gap to show training result
+        dropout: Dropout, probability to keep units
+        strides: list with 8 elements
+            1. conv layer 1 stride size for mean net
+            2. max_pool layer 1 ksize and strides size for mean net
+            3. conv layer 2 stride size for mean net
+            4. max_pool layer 2 ksize and strides size for mean net
+            5. conv layer 1 stride size for var net
+            6. max_pool layer 1 ksize and strides size for var net
+            7. conv layer 2 stride size for var net
+            8. max_pool layer 2 ksize and strides size for var net
+        """
+        # Initializing network
         self.__construct_net__(strides=strides)
         self.__define_measure__()
         self.__optimization__(learning_rate=learning_rate)
@@ -126,36 +272,40 @@ class mean_var_net():
         with tf.Session() as sess:
             sess.run(init)
             step = 1
-            train_writer = tf.summary.FileWriter('../summary/train',
+            # Record train data
+            train_writer = tf.summary.FileWriter('./summary_data/train',
                                                   sess.graph)
 
             # Keep training until reach max iterations
             while step * batch_size <= training_iters:
                 batch_x, batch_y = self.data.train.next_batch(batch_size)
                 # Run optimization op (backprop)
-                mean,var = sess.run([self.mean_net.out,self.var_net.out], feed_dict = {self.x: batch_x,
-                                                                self.y: batch_y,
-                                                                self.keep_prob: dropout})
 
-                summary, _ = sess.run([merged, self.apply_gradients], feed_dict={self.x: batch_x,
-                                                                                 self.y: batch_y,
-                                                                                 self.keep_prob: dropout,
-                                                                                 self.learning_rate: learning_rate,
-                                                                                 self.sample_size: batch_y.shape[0]})
+                summary, _ = sess.run([merged, self.apply_gradients],
+                                       feed_dict={self.x: batch_x,
+                                                  self.y: batch_y,
+                                                  self.keep_prob: dropout,
+                                                  self.learning_rate: learning_rate,
+                                                  self.sample_size: batch_y.shape[0]})
 
-                sess.run(self.validation_cost, feed_dict={self.x: self.data.validation.images,
-                                                          self.y: self.data.validation._labels,
-                                                          self.sample_size: self.data.validation._labels.shape[0],
-                                                          self.keep_prob: 1.})
+
                 train_writer.add_summary(summary, step)
                 # display training intermediate result
 
                 if step % display_step == 0:
                     # Calculate batch loss and accuracy
-                    loss, acc = sess.run([self.cost,self.accuracy], feed_dict={self.x: batch_x,
-                                                                               self.y: batch_y,
-                                                                               self.sample_size: batch_y.shape[0],
-                                                                               self.keep_prob: 1.})
+                    loss, acc = sess.run([self.cost,self.accuracy],
+                                          feed_dict={self.x: batch_x,
+                                                     self.y: batch_y,
+                                                     self.sample_size: batch_y.shape[0],
+                                                     self.keep_prob: 1.})
+                    # Calculate validation loss
+                    sess.run(self.validation_cost,
+                             feed_dict={self.x: self.data.validation.images,
+                                        self.y: self.data.validation._labels,
+                                        self.sample_size: self.data.validation._labels.shape[0],
+                                        self.keep_prob: 1.})
+
                     print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + \
                           "{:.6f}".format(loss) + ", Training Accuracy= " + \
                           "{:.5f}".format(acc))
@@ -173,6 +323,7 @@ if __name__ == '__main__':
     mnist.test.labels.setflags(write = 1)
     mnist.train.labels.setflags(write = 1)
     mnist.validation.labels.setflags(write=1)
+    # Change labels 0~9 to {0,1}
     mnist.train.labels[mnist.train.labels <= 4] = 0
     mnist.train.labels[mnist.train.labels > 4] = 1
     mnist.test.labels[mnist.test.labels <= 4] = 0
@@ -183,5 +334,6 @@ if __name__ == '__main__':
     mnist.train._labels = np.eye(2)[mnist.train.labels]
     mnist.test._labels = np.eye(2)[mnist.test.labels]
     mnist.validation._labels = np.eye(2)[mnist.validation.labels]
+    # Construct and train network
     mvnn = mean_var_net(data=mnist)
-    mvnn.train_net(training_iters=20000, learning_rate =0.001,batch_size=128, display_step=2)
+    mvnn.train_net(training_iters=40000, learning_rate =0.001,batch_size=128, display_step=10)
